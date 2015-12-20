3e3fb19301787679e7f0daf6b4206fe294b451b7
leslie.li <leslie.li@live.com>
2015-12-20 15:30:49 +0800

    fix

--- src/main/webapp/static/widget/newslist/newslist.js
+++ src/main/webapp/static/widget/newslist/newslist.js
@@ -10,27 +10,30 @@ var app = module.exports = function(opt) {
 		addEvent : function(){
 			var _this = this;
 			$(opt.dom).click(function(){
-				var url;
-				$('.J_listLoadMore').addClass('loading');
+				var url,
+					$J_loadMore = $('.J_listLoadMore');
 				if($('.J_newsListNavBar').length == 0 || $('.J_newsListNavBar .firstList').hasClass('active')){
 					url = opt.newsUrl
 				}else{
 					url = opt.pageCateUrl + $('.J_newsListNavBar .active').attr('data')
 				}
-				_this.getList(url,{
-					offset : $('#listWrap').children(':last').attr('data'),
-					d : 'next'
-				},function(data){
-					if(data.code == 0){
-						if(data.data.length){
-							$('.J_listLoadMore').removeClass('loading no-data');
-							var result = Template.parse(_this.listTemplate,{data:data});
-							$("#listWrap").append(result);
-						}else{
-							$('.J_listLoadMore').removeClass('loading').addClass('no-data');
-						}
-			   		}
-				});
+				if(!$J_loadMore.hasClass('no-data')){
+					$J_loadMore.addClass('loading');
+					_this.getList(url,{
+						offset : $('#listWrap').children(':last').attr('data'),
+						d : 'next'
+					},function(data){
+						if(data.code == 0){
+							if(data.data.length){
+								$J_loadMore.removeClass('loading no-data');
+								var result = Template.parse(_this.listTemplate,{data:data});
+								$("#listWrap").append(result);
+							}else{
+								$J_loadMore.removeClass('loading').addClass('no-data');
+							}
+				   		}
+					});
+				}
 			});
 			$('.J_newsListNavBar').length && $('.J_newsListNavBar').delegate('.tab','click',function(e){
 				var url = ($(this).hasClass('firstList') ? opt.newsUrl : opt.pageCateUrl) + $(this).attr('data')

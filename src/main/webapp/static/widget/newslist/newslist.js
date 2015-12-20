d5aa5c1f19de9829f9c84050f94b61fa0b2cdd39
leslie.li <leslie.li@live.com>
2015-12-20 15:21:54 +0800

    fix bug

--- src/main/webapp/static/widget/newslist/newslist.js
+++ src/main/webapp/static/widget/newslist/newslist.js
@@ -11,7 +11,8 @@ var app = module.exports = function(opt) {
 			var _this = this;
 			$(opt.dom).click(function(){
 				var url;
-				if($('.J_newsListNavBar .firstList').hasClass('active')){
+				$('.J_listLoadMore').addClass('loading');
+				if($('.J_newsListNavBar').length == 0 || $('.J_newsListNavBar .firstList').hasClass('active')){
 					url = opt.newsUrl
 				}else{
 					url = opt.pageCateUrl + $('.J_newsListNavBar .active').attr('data')
@@ -21,15 +22,20 @@ var app = module.exports = function(opt) {
 					d : 'next'
 				},function(data){
 					if(data.code == 0){
-						var result = Template.parse(_this.listTemplate,{data:data});
-						$("#listWrap").append(result);
+						if(data.data.length){
+							$('.J_listLoadMore').removeClass('loading no-data');
+							var result = Template.parse(_this.listTemplate,{data:data});
+							$("#listWrap").append(result);
+						}else{
+							$('.J_listLoadMore').removeClass('loading').addClass('no-data');
+						}
 			   		}
 				});
 			});
 			$('.J_newsListNavBar').length && $('.J_newsListNavBar').delegate('.tab','click',function(e){
 				var url = ($(this).hasClass('firstList') ? opt.newsUrl : opt.pageCateUrl) + $(this).attr('data')
+				$('.J_listLoadMore').removeClass('loading no-data');
 				_this.getList(url,{
-					offset : $('#listWrap').children(':last').attr('data'),
 					d : 'next'
 				},function(data){
 					if(data.code == 0){
